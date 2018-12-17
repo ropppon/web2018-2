d9466d483fc30c01b1eb0e628267e31539665d86
zm16641 <zm16641@josai.ac.jp>
2018-12-16 21:00:34 -0500

    message

--- ex07-12.js
+++ ex07-12.js
@@ -1,5 +1,6 @@
+
 function nazo(){
-if(document.getElementById('answer').value='夢'){
+ if(document.getElementById('answer').value='夢'){
   alert('あたり！');
 }else{
   alert('はずれ！');
--- ex07-4.js
+++ ex07-4.js
@@ -1,1 +1,7 @@
-?
+function ada(){
+var w = Number(document.getElementById("height").value);
+var y = Number(document.getElementById("weight").value);
+      alert( y / ( (w / 100) * (w / 100) ) );
+    }
+   
+              
